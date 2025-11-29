18abb6edd1917056e278e4accb123ab8c485a95b
Your Name <you@example.com>
2025-11-28 23:28:08 -0600

    creacion de table books,es uan tipo liblioteca

---
+++ database/migrations/2025_11_29_050836_create_books_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->foreignId('author_id')->constrained()->cascadeOnDelete();
+            $table->year('year');
+            $table->string('genre');
+            $table->integer('stock')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+
+};
