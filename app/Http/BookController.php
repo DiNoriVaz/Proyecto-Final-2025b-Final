d01e66316a5181c4a999c16ae762225bec71bc88
Your Name <you@example.com>
2025-11-29 21:44:52 -0600

    añadir BookController2.php

--- app/Http/BookController.php
+++ app/Http/BookController.php
@@ -3,3 +3,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Book;
 use App\Models\Author;
+use App\Models\User;
+
+class BookController extends Controller {
+public function index() {
+return view('books.index', ['books' => Book::with('author')->get()]);
+}
+public function create() {
+return view('books.create', ['authors' => Author::all()]);
+}
+
+}
