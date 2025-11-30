7e37028fa41b02648ce6e043ed90f5c01056fdf5
Your Name <you@example.com>
2025-11-29 21:55:05 -0600

    añadir BookController3.php

--- app/Http/BookController.php
+++ app/Http/BookController.php
@@ -1,16 +1,49 @@
 <?php
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-use App\Http\Book;
+use App\Models\Book;
 use App\Models\Author;
-use App\Models\User;
-
+use Illuminate\Http\Request;
 class BookController extends Controller {
-public function index() {
-return view('books.index', ['books' => Book::with('author')->get()]);
+    public function index() {
+    return view('books.index', ['books' => Book::with('author')->get()]);
+    }
+    public function create() {
+    return view('books.create', ['authors' => Author::all()]);
+    }
+    public function store(Request $request) {
+    $request->validate([
+    'title' => 'required',
+    'author_id' => 'required',
+    'year' => 'required',
+    'genre' => 'required'
+    ]);
+
+
+Book::create($request->all());
+return redirect()->route('books.index');
 }
-public function create() {
-return view('books.create', ['authors' => Author::all()]);
-}
+
+    public function edit(Book $book) {
+    return view('books.edit', ['book' => $book, 'authors' => Author::all()]);
+    }
+
+
+    public function update(Request $request, Book $book) {
+    $request->validate([
+    'title' => 'required',
+    'author_id' => 'required'
+    ]);
+
+
+    $book->update($request->all());
+    return redirect()->route('books.index');
+    }
+
+
+    public function destroy(Book $book) {
+    $book->delete();
+    return redirect()->route('books.index');
+    }
+
 
 }
