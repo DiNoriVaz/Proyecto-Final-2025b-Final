537d043c2c754a8a24fbe40b01c2158432b2b223
Your Name <you@example.com>
2025-11-29 21:22:38 -0600

    añadir authorcontroller par1

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -7,6 +7,13 @@ class Loan extends Model {
 use HasFactory;
 protected $fillable = ['user_id','book_id','loan_date','return_date'];
 
+    public function user() {
+    return $this->belongsTo(User::class);
+    }
 
+
+    public function book() {
+    return $this->belongsTo(Book::class);
+    }
 
 }
